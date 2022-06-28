d5301788f2aa59a44939720c4a3b929d9356bb63
heitorgusan <heitorgusan@gmail.com>
2022-06-28 19:45:48 -0300

    carrinho adicionar itens repetidos somar na quantidade corretamente

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -13,28 +13,38 @@ import { carrinho } from '../environments/carrinho';
 })
 export class InicioComponent implements OnInit {
 
-  public listaProdutos:ProdutoModel[] = [];
-  constructor(private router: Router,private produtoService: ProdutoService){} //private cabecalho: CabecalhoComponent) { }
+  public listaProdutos: ProdutoModel[] = [];
+  constructor(private router: Router, private produtoService: ProdutoService) { } //private cabecalho: CabecalhoComponent) { }
 
   ngOnInit(): void {
 
-    this.produtoService.buscarProdutos().subscribe((lista:ProdutoModel[])=>{
+    this.produtoService.buscarProdutos().subscribe((lista: ProdutoModel[]) => {
       this.listaProdutos = lista;
       console.log(this.listaProdutos);
     });
 
 
   }
-  adicionarCarrinho(produto : ProdutoModel){
-    const itemCarrinho:ItemCarrinho = new ItemCarrinho();
+  adicionarCarrinho(produto: ProdutoModel) {
+    //Verificando se o produto já existe no carinho
+    for (let item of carrinho) {
+      console.log(produto);
+      console.log(item.produto);
+      if (produto.idProduto == item.produto.idProduto) {
+        item.qtde++;
+        return;
+      }
+    }
+    const itemCarrinho: ItemCarrinho = new ItemCarrinho();
     itemCarrinho.produto = produto;
-    itemCarrinho.qtde=1;
+    itemCarrinho.qtde = 1;
     carrinho.push(itemCarrinho);
 
-    //this.cabecalho.ngOnInit();
-}
-exibirCarrossel():Boolean{
 
-  return true;
-}
+    //this.cabecalho.ngOnInit();
+  }
+  exibirCarrossel(): Boolean {
+
+    return true;
+  }
 }

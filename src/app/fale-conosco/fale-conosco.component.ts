32dabee62e50192926c3db451ba74af08b3fcc00
Patricia <patty.rogai@gmail.com>
2022-06-30 22:18:18 -0300

    alert fale conosco

--- src/app/fale-conosco/fale-conosco.component.ts
+++ src/app/fale-conosco/fale-conosco.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-fale-conosco',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fale-conosco.component.css']
 })
 export class FaleConoscoComponent implements OnInit {
+nome:string=""
+email:string=""
+mensagem:string=""
 
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+  }
+mensagemEnviadaComSucesso(){
+  Swal.fire({
+    position: 'center',
+    icon: 'success',
+    title: 'Mensagem enviada com sucesso!',
+    showConfirmButton: false,
+    timer: 1500
+  })
+  this.limparCampo()
+}
+limparCampo(){
+  this.nome=""
+  this.email=""
+  this.mensagem=""
+}
 }
